f3d856d7f16c583687b3b631ea79da912f8584c3
waleedalikhan <waleedomore@gmail.com>
2025-10-05 01:52:02 +0500

    Tailwind message removed

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,10 +10,6 @@ const Home = () => {
   return (
     <>
       <article className="page">
-        {/* Tailwind test - this should show a blue background if Tailwind is working */}
-        <div className="bg-blue-500 text-white p-4 m-4 rounded-lg">
-          <p>Tailwind CSS is working! This is a test element.</p>
-        </div>
         <HeroSection />
         <TopVillas />
         <Regions />
